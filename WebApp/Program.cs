eef0a93dbeb2aa06e9032fa1e674c065e9b930e0
Smilodon <Smilodon@LAPTOP-SMILO>
2024-03-04 14:25:24 +0100

    Projektdateien hinzufügen.

---
+++ WebApp/Program.cs
@@ -0,0 +1,24 @@
+namespace WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Konfiguration der Anwendung als MVC-Anwendung
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.UseRouting();
+            app.MapControllerRoute(
+                               name: "default",
+                                              pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
---
+++ WebApp/ReadMe.txt
@@ -0,0 +1,17 @@
+﻿Von leerer Web-App zu vollständiger Web-App
+-Verzeichnesstruktur anlegen
+--Controllers
+--Models
+--Views
+
+-Controller anlegen (z.B. HomeController)
+-View anlegen (z.B. Index.cshtml)
+
+- Web APP als MVC Anwendung festlegen
+-- builder.Services.AddControllersWithViews();
+
+- Routing aktivieren
+-- app.UseRouting();
+
+-Standartroute festlegen
+--app.MapControllerRoute(name: "default",pattern: "{controller=Home}/{action=Index}/{id?}");
