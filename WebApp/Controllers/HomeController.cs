a0d770277d7073987179c8fe015809268cd84dea
ex3tick <schwabiigofit@gmail.com>
2024-03-06 22:27:31 +0100

    new create button

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -7,32 +7,43 @@ namespace WebApp.Controllers
     public class HomeController : Controller
     {
         private readonly CrudInterfaceServices _service = new CrudInterfaceServices();
+
         public IActionResult Index()
         {
-            List<Boot>  Boote = _service.GetAllBoats();
+            List<Boot> Boote = _service.GetAllBoats();
             return View(Boote);
         }
+
         public IActionResult Create()
         {
             return View();
         }
+
         public IActionResult Detail(int bid)
         {
-          
-           Boot boot = _service.GetBoatById(bid);
-            
+
+            Boot boot = _service.GetBoatById(bid);
+
             return View(boot);
         }
+
         public IActionResult Delete(int bid)
         {
             _service.DeleteBoat(bid);
             return View();
         }
+
         public IActionResult Edit(int bid)
         {
             Boot boot = _service.GetBoatById(bid);
             
-            return View(boot);
+            return View(boot); 
         }
+        
+        
     }
-}
+        
+    }
+
+
+
